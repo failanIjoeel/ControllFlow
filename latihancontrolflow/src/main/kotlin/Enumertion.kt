275df4a5091571a23bb129e0c0c604dbb0628a99
failanIjoeel <failanrahsya90@gmail.com>
2020-08-31 17:00:48 +0700

    Control Flow

---
+++ latihancontrolflow/src/main/kotlin/Enumertion.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val getPlayerLevel = (playerID.Failan.value)
+    print("Player Level is $getPlayerLevel")
+}
+
+///Enumeration Class
+enum class playerID( val value: Int){
+    Failan(15),
+    Ijoeel(16),
+    Rahsya(74)
+}
